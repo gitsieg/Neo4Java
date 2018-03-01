5ed7b9d1b2df4ee70eaeaacc624c7d22fbb0e689
Nikolai Fosså <nikolaidime@gmail.com>
2018-03-01 20:21:04 +0100

    Add graph successfully created with telemark.geojson data

--- src/main/java/Neo4Demo.java
+++ src/main/java/Neo4Demo.java
@@ -1,27 +1,27 @@
-import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.neo4j.cypher.internal.ExecutionEngine;
-import org.neo4j.cypher.internal.compatibility.v2_3.StringInfoLogger;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.io.IOUtils;
 
 import java.io.*;
-import java.util.Iterator;
 
 public class Neo4Demo {
 
-    private static final File dbDir = new File("/var/lib/neo4j/data/databases/javatest.db");
+
+    private static final File DATABASE_DIRECTORY = new File("/var/lib/neo4j/data/databases/javatest.db");
     // Predefined queries.
     private static final String DETACH_DELETE = "MATCH (n) DETACH DELETE n";
 
     public enum NodeType implements Label {
-        Koordinat, Kommune, Fylke, ;
+        Koordinat,
+        Kommune,
+        Fylke;
     }
 
     public enum RelationType implements RelationshipType {
-        Has, BelongsTo;
+        HAR_KOMMUNE,
+        KOMMUNE_GRENSE;
     }
 
 
@@ -31,20 +31,61 @@ public class Neo4Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JSONObject jsonDump = new JSONObject(readFile().toString());
-        System.out.println(jsonDump.toString(2));
+        JSONObject fylkeData = new JSONObject(readFile().toString());
+
+
+        JSONArray kommuneFeatures = fylkeData.getJSONObject("administrative_enheter_kommune")
+                .getJSONArray("features");
+
+
+//        System.out.println(kommuneFeatures.toString(2));
 
         GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
-        GraphDatabaseBuilder builder = dbFactory.newEmbeddedDatabaseBuilder(dbDir);
+        GraphDatabaseBuilder builder = dbFactory.newEmbeddedDatabaseBuilder(DATABASE_DIRECTORY);
         GraphDatabaseService graphdb = builder.newGraphDatabase();
         Result result;
 
-        // All access to graphdb goes in here
-//        try (Transaction tx = graphdb.beginTx()) {
-//
-//
-//        }
+        //TODO: Fix neo4j process server failing to stop with systemctl stop neo4j.... -_-
 
+        // All access to graphdb goes in here
+        try (Transaction tx = graphdb.beginTx()) {
+            graphdb.execute(DETACH_DELETE);
+
+            Node telemark = graphdb.createNode(NodeType.Fylke);
+            telemark.setProperty("Navn", "Telemark");
+            telemark.setProperty("Kode", "NO-08");
+
+            for (int i = 0; i < kommuneFeatures.length(); i++) {
+                JSONObject kommune = kommuneFeatures.getJSONObject(i);
+                JSONObject kommuneProperties = kommune.getJSONObject("properties");
+
+                // Lag kommunennode
+                String kommuneNavn = kommuneProperties.getJSONArray("navn").getJSONObject(0).getString("navn");
+                String kommuneNr = kommuneProperties.getString("kommunenummer");
+
+                Node kommuneNode = graphdb.createNode(NodeType.Kommune);
+                kommuneNode.setProperty("KommuneNavn", kommuneNavn);
+                kommuneNode.setProperty("Kommunenr", kommuneNavn);
+
+                telemark.createRelationshipTo(kommuneNode, RelationType.HAR_KOMMUNE);
+
+
+                // Behandle koordinater og lenk
+                JSONArray koordinater = kommune.getJSONObject("geometry").getJSONArray("coordinates").getJSONArray(0);
+                JSONArray punkt = null;
+                for (int j = 0; j < koordinater.length(); j++) {
+                    punkt = koordinater.getJSONArray(i);
+                    Node koordinatSett = graphdb.createNode(NodeType.Koordinat);
+                    koordinatSett.setProperty("lat", punkt.getInt(0));
+                    koordinatSett.setProperty("lon", punkt.getInt(1));
+                    kommuneNode.createRelationshipTo(koordinatSett, RelationType.KOMMUNE_GRENSE);
+                }
+
+            }
+            tx.success();
+        }
+        graphdb.shutdown();
+    }
 
         //
 //        try (Transaction tx = graphdb.beginTx()) {
@@ -92,11 +133,15 @@ public class Neo4Demo {
 //
 //        }
 //        graphdb.shutdown();
-    }
+//    }
 
+    /**
+     * Reads a file with character data.
+     * @return StringBuil
+     */
     public static StringBuilder readFile() {
         StringBuilder builder = new StringBuilder();
-        try (FileInputStream stream = new FileInputStream("/home/gitsieg/IdeaProjects/Neo4Java/src/main/java/bo.geojson")) {
+        try (FileInputStream stream = new FileInputStream("/home/gitsieg/IdeaProjects/Neo4Java/src/main/java/telemark.geojson")) {
             InputStreamReader streamReader = new InputStreamReader(stream);
             BufferedReader reader = new BufferedReader(streamReader);
 
