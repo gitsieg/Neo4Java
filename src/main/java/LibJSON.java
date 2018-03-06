cc8d1acff21179930dab6ed1786040964dbaea0d
Kristian Robertsen <kristian.robertsen@gmail.com>
2018-03-06 12:06:17 +0100

    Legg til fylke-ressurser; Opprett JSON libs

---
+++ src/main/java/LibJSON.java
@@ -0,0 +1,53 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.neo4j.graphdb.Node;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LibJSON {
+    static HashMap<String, String> getFylker(File file) {
+        HashMap<String, String> fylker = new HashMap<>();
+
+        JSONObject json_file_fylker = new JSONObject(readJSON(file).toString());
+
+        JSONArray json_content_fylker = json_file_fylker.getJSONArray("fylker");
+        String prefix = json_file_fylker.getString("ISO_prefix");
+
+        for (int i = 0; i < json_content_fylker.length(); i++) {
+            JSONObject fylke = json_content_fylker.getJSONObject(i);
+            fylker.put(fylke.getString("nr"), fylke.getString("navn"));
+        }
+
+
+        return fylker;
+    }
+
+
+
+
+
+
+
+/* ---------- Helpers ---------- */
+
+    private static StringBuilder readJSON(File file) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(file)));
+
+            for (String line = reader.readLine(); line != null; line = reader.readLine())
+                builder.append(line);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder;
+    }
+}
+
+
