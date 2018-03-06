e1647529666407ae14d88438337d2cac326c8bed
Nikolai Fosså <nikolaidime@gmail.com>
2018-03-06 12:54:55 +0100

    Rename json resources

--- src/main/java/Neo4Demo.java
+++ src/main/java/Neo4Demo.java
@@ -6,8 +6,6 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 
 public class Neo4Demo {
 
@@ -19,12 +17,14 @@ public class Neo4Demo {
     public enum NodeType implements Label {
         Koordinat,
         Kommune,
-        Fylke;
+        Fylke,
+        Land
     }
 
     public enum RelationType implements RelationshipType {
         HAR_KOMMUNE,
-        KOMMUNE_GRENSE;
+        KOMMUNE_GRENSE,
+        HAR_FYLKE
     }
 
 
@@ -34,11 +34,10 @@ public class Neo4Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JSONObject fylkeData = new JSONObject(readFile().toString());
 
 
-        JSONArray kommuneFeatures = fylkeData.getJSONObject("administrative_enheter_kommune")
-                .getJSONArray("features");
+//        JSONArray kommuneFeatures = fylkeData.getJSONObject("administrative_enheter_kommune")
+//                .getJSONArray("features");
 
 
         GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
@@ -46,40 +45,32 @@ public class Neo4Demo {
         GraphDatabaseService graphdb = builder.newGraphDatabase();
         registerShutdownHook(graphdb);
 
-
         // All access to graphdb goes in here
         try (Transaction tx = graphdb.beginTx()) {
             graphdb.execute(DETACH_DELETE);
 
-            Node telemark = graphdb.createNode(NodeType.Fylke);
-            telemark.setProperty("navn", "Telemark");
-            telemark.setProperty("nr", "NO-08");
+            // Opprett toppnivå i grafen
+            Node nodeNorge = graphdb.createNode(NodeType.Land);
+            nodeNorge.setProperty("navn", "Norge");
+            nodeNorge.setProperty("kode", "NO");
 
-            for (int i = 0; i < kommuneFeatures.length(); i++) {
-                JSONObject kommune = kommuneFeatures.getJSONObject(i);
-                JSONObject kommuneProperties = kommune.getJSONObject("properties");
+            ArrayList<Node> fylker = getFylker(graphdb);
 
-                // Lag kommunennode
-                String komNavn = kommuneProperties.getJSONArray("navn").getJSONObject(0).getString("navn");
-                String komNr = kommuneProperties.getString("kommunenummer");
 
-                Node komNode = graphdb.createNode(NodeType.Kommune);
-                komNode.setProperty("navn", komNavn);
-                komNode.setProperty("nr", komNavn);
+            // Peker til mappe som inneholder fylker.
+            File res = new File("./src/res/json/fylker");
+            System.out.println(res.getAbsolutePath());
+            System.out.println(res.isDirectory());
+            System.out.println(res.exists());
 
-                telemark.createRelationshipTo(komNode, RelationType.HAR_KOMMUNE);
+            File[] fylkeFiles = res.listFiles();
 
-                // Behandle koordinater og lenk
-                JSONArray koordinater = kommune.getJSONObject("geometry").getJSONArray("coordinates").getJSONArray(0);
-                for (int j = 0; j < koordinater.length(); j++) {
-                    JSONArray punkt = koordinater.getJSONArray(j);
-                    Node koordinatSett = graphdb.createNode(NodeType.Koordinat);
-                    koordinatSett.setProperty("lat", punkt.getDouble(0));
-                    koordinatSett.setProperty("lon", punkt.getDouble(1));
-                    komNode.createRelationshipTo(koordinatSett, RelationType.KOMMUNE_GRENSE);
-                }
+            for (File file : fylkeFiles) {
 
             }
+
+
+
             tx.success();
             tx.close();
         }
@@ -91,9 +82,9 @@ public class Neo4Demo {
      *
      * @return StringBuil
      */
-    public static StringBuilder readFile() {
+    public static StringBuilder readFile(String fylkenavn) {
         StringBuilder builder = new StringBuilder();
-        try (FileInputStream stream = new FileInputStream("/home/gitsieg/IdeaProjects/Neo4Java/src/main/java/telemark.geojson")) {
+        try (FileInputStream stream = new FileInputStream("src/main/java/fylker/" + fylkenavn + ".geojson")) {
             InputStreamReader streamReader = new InputStreamReader(stream);
             BufferedReader reader = new BufferedReader(streamReader);
 
@@ -101,8 +92,6 @@ public class Neo4Demo {
             while ((line = reader.readLine()) != null) {
                 builder.append(line);
             }
-
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -125,83 +114,84 @@ public class Neo4Demo {
 
     /**
      * Sets graph topleveldata
+     *
      * @param grapdb
      * @return
      */
-    private ArrayList<Node> setFylker(GraphDatabaseService grapdb) {
-
+    private static ArrayList<Node> getFylker(GraphDatabaseService grapdb) {
+        String fylkenr = "fylkenr", navn = "navn";
         ArrayList<Node> listFylke = new ArrayList<>();
         Node nodeOstfold = grapdb.createNode(NodeType.Fylke);
-        nodeOstfold.setProperty("fylkenr", "NO-01");
-        nodeOstfold.setProperty("name", "Østfold");
+        nodeOstfold.setProperty(fylkenr, "NO-01");
+        nodeOstfold.setProperty(navn, "Østfold");
 
         Node nodeAkershus = grapdb.createNode(NodeType.Fylke);
-        nodeAkershus.setProperty("fylkenr", "NO-02");
-        nodeAkershus.setProperty("name", "Akershus");
+        nodeAkershus.setProperty(fylkenr, "NO-02");
+        nodeAkershus.setProperty(navn, "Akershus");
 
         Node nodeOslo = grapdb.createNode(NodeType.Fylke);
-        nodeOslo.setProperty("fylkenr", "NO-03");
-        nodeOslo.setProperty("name", "Oslo");
+        nodeOslo.setProperty(fylkenr, "NO-03");
+        nodeOslo.setProperty(navn, "Oslo");
 
         Node nodeHedmark = grapdb.createNode(NodeType.Fylke);
-        nodeHedmark.setProperty("fylkenr", "NO-04");
-        nodeHedmark.setProperty("name", "Hedmark");
+        nodeHedmark.setProperty(fylkenr, "NO-04");
+        nodeHedmark.setProperty(navn, "Hedmark");
 
         Node nodeOppland = grapdb.createNode(NodeType.Fylke);
-        nodeOppland.setProperty("fylkenr", "NO-05");
-        nodeOppland.setProperty("name", " Oppland");
+        nodeOppland.setProperty(fylkenr, "NO-05");
+        nodeOppland.setProperty(navn, "Oppland");
 
         Node nodeBuskerud = grapdb.createNode(NodeType.Fylke);
-        nodeBuskerud.setProperty("fylkenr", "NO-06");
-        nodeBuskerud.setProperty("name", "Buskerud");
+        nodeBuskerud.setProperty(fylkenr, "NO-06");
+        nodeBuskerud.setProperty(navn, "Buskerud");
 
-        Node nodeVestfold= grapdb.createNode(NodeType.Fylke);
-        nodeVestfold.setProperty("fylkenr", "NO-07");
-        nodeVestfold.setProperty("name", "Vestfold");
+        Node nodeVestfold = grapdb.createNode(NodeType.Fylke);
+        nodeVestfold.setProperty(fylkenr, "NO-07");
+        nodeVestfold.setProperty(navn, "Vestfold");
 
         Node nodeTelemark = grapdb.createNode(NodeType.Fylke);
-        nodeTelemark.setProperty("fylkenr", "NO-08");
-        nodeTelemark.setProperty("name", "Telemark");
+        nodeTelemark.setProperty(fylkenr, "NO-08");
+        nodeTelemark.setProperty(navn, "Telemark");
 
         Node nodeAustAgder = grapdb.createNode(NodeType.Fylke);
-        nodeAustAgder.setProperty("fylkenr", "NO-09");
-        nodeAustAgder.setProperty("name", "Aust-Agder");
+        nodeAustAgder.setProperty(fylkenr, "NO-09");
+        nodeAustAgder.setProperty(navn, "Aust-Agder");
 
-        Node nodeVestAgder= grapdb.createNode(NodeType.Fylke);
-        nodeVestAgder.setProperty("fylkenr", "NO-10");
-        nodeVestAgder.setProperty("name", "Vest-Agder");
+        Node nodeVestAgder = grapdb.createNode(NodeType.Fylke);
+        nodeVestAgder.setProperty(fylkenr, "NO-10");
+        nodeVestAgder.setProperty(navn, "Vest-Agder");
 
         Node nodeRogaland = grapdb.createNode(NodeType.Fylke);
-        nodeRogaland.setProperty("fylkenr", "NO-11");
-        nodeRogaland.setProperty("name", "Rogaland");
+        nodeRogaland.setProperty(fylkenr, "NO-11");
+        nodeRogaland.setProperty(navn, "Rogaland");
 
         Node nodeHordaland = grapdb.createNode(NodeType.Fylke);
-        nodeHordaland.setProperty("fylkenr", "NO-12");
-        nodeHordaland.setProperty("name", "Hordaland");
+        nodeHordaland.setProperty(fylkenr, "NO-12");
+        nodeHordaland.setProperty(navn, "Hordaland");
 
         Node nodeSognOgFjordane = grapdb.createNode(NodeType.Fylke);
-        nodeSognOgFjordane.setProperty("fylkenr", "NO-14");
-        nodeSognOgFjordane.setProperty("name", "Sogn og Fjordane");
+        nodeSognOgFjordane.setProperty(fylkenr, "NO-14");
+        nodeSognOgFjordane.setProperty(navn, "Sogn og Fjordane");
 
         Node nodeMoreOgRomsdal = grapdb.createNode(NodeType.Fylke);
-        nodeMoreOgRomsdal.setProperty("fylkenr", "NO-15");
-        nodeMoreOgRomsdal.setProperty("name", "Møre og Romsdal");
+        nodeMoreOgRomsdal.setProperty(fylkenr, "NO-15");
+        nodeMoreOgRomsdal.setProperty(navn, "Møre og Romsdal");
 
         Node nodeTrondelag = grapdb.createNode(NodeType.Fylke);
-        nodeTrondelag.setProperty("fylkenr", "NO-50");
-        nodeTrondelag.setProperty("name", "Trøndelag");
+        nodeTrondelag.setProperty(fylkenr, "NO-50");
+        nodeTrondelag.setProperty(navn, "Trøndelag");
 
         Node nodeNordland = grapdb.createNode(NodeType.Fylke);
-        nodeNordland.setProperty("fylkenr", "NO-18");
-        nodeNordland.setProperty("name", "Nordland");
+        nodeNordland.setProperty(fylkenr, "NO-18");
+        nodeNordland.setProperty(navn, "Nordland");
 
         Node nodeTroms = grapdb.createNode(NodeType.Fylke);
-        nodeTroms.setProperty("fylkenr", "NO-19");
-        nodeTroms.setProperty("name", "Troms");
+        nodeTroms.setProperty(fylkenr, "NO-19");
+        nodeTroms.setProperty(navn, "Troms");
 
         Node nodeFinnmark = grapdb.createNode(NodeType.Fylke);
-        nodeFinnmark.setProperty("fylkenr", "NO-20");
-        nodeFinnmark.setProperty("name", "Finnmark");
+        nodeFinnmark.setProperty(fylkenr, "NO-20");
+        nodeFinnmark.setProperty(navn, "Finnmark");
 
         listFylke.add(nodeOstfold);
         listFylke.add(nodeAkershus);
@@ -223,7 +213,5 @@ public class Neo4Demo {
         listFylke.add(nodeFinnmark);
 
         return listFylke;
-
-
     }
 }
